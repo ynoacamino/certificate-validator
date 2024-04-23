96916d11dbaea2aad02adb6d38ed53ac4bd67430
Yenaro Joel Noa Camino <ynoacamino@gmail.com>
2024-04-23 00:13:50 -0500

    Add PrismaClient and OnModuleInit to PrismaService

--- apps/server/src/prisma/prisma.service.ts
+++ apps/server/src/prisma/prisma.service.ts
@@ -1,8 +1,9 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, OnModuleInit } from '@nestjs/common';
+import { PrismaClient } from '@prisma/client';
 
 @Injectable()
-export class PrismaService {
-  constructor() {
-    console.log('PrismaService instantiated');
+export class PrismaService extends PrismaClient implements OnModuleInit {
+  async onModuleInit() {
+    await this.$connect();
   }
 }
