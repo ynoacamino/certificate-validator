ba775df7f7f921bf58093b50084cb6950ee68b7f
Yenaro Joel Noa Camino <ynoacamino@gmail.com>
2024-04-28 18:06:14 -0500

    Update server and client URLs in globalConfig.ts

--- apps/client/src/data/globalConfig.ts
+++ apps/client/src/data/globalConfig.ts
@@ -1,3 +1,3 @@
-export const SERVER_URL = `http://localhost:${process.env.SERVER_PORT}` || 'http://localhost:3001';
+export const SERVER_URL = `http://localhost:${process.env.NEXT_PUBLIC_SERVER_PORT}` || 'http://localhost:3001';
 
-export const CLIENT_URL = `http://localhost:${process.env.PORT}` || 'http://localhost:3000';
+export const CLIENT_URL = process.env.PORT ? `http://localhost:${process.env.PORT}` : 'http://localhost:3000';
