a197fb3f233a0142016eb4906eb1f73ff9c97c1d
Yenaro Joel Noa Camino <ynoacamino@gmail.com>
2024-04-28 22:00:40 -0500

    Update CORS origin in main.ts to allow all origins

--- apps/server/src/main.ts
+++ apps/server/src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'https://certificate-validator-sigma.vercel.app/',
+    origin: '*',
     methods: 'GET, POST, PUT, DELETE',
   });
 
